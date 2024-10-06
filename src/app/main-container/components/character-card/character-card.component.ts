f408bf40540d4e7718a8c18dfeb4bbb5d16ba6e1
Alexis Miranda <alexis_mir@hotmail.com>
2024-10-06 07:08:52 -0300

    fix cards

--- src/app/main-container/components/character-card/character-card.component.ts
+++ src/app/main-container/components/character-card/character-card.component.ts
@@ -5,7 +5,7 @@ import {
   inject,
   input,
 } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Character } from '@app/models';
 import { GlobalStore } from '@app/store';
 import { MatButtonModule } from '@angular/material/button';
@@ -15,22 +15,23 @@ import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-character-card',
   standalone: true,
-  imports: [NgOptimizedImage, MatCardModule, MatButtonModule, MatIconModule],
+  imports: [
+    NgOptimizedImage,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink,
+  ],
   templateUrl: './character-card.component.html',
   styleUrl: './character-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CharacterCardComponent {
   character = input.required<Character>();
-  router = inject(Router);
   readonly store = inject(GlobalStore);
   route = inject(ActivatedRoute);
 
   removeCharacter(id: number) {
     this.store.removeCharacter(id);
-  }
-
-  editCharacter(id: number) {
-    this.router.navigate([id + '/edit'], { relativeTo: this.route });
   }
 }
